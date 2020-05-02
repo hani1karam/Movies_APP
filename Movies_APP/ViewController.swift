7bb21d48f6278aa56db45b6b8e63baa13bf435de
hani1karam <hanykaram63@gmail.com>
2020-05-02 22:00:28 +0200

    Initial Commit

---
+++ Movies_APP/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movies_APP
+//
+//  Created by Hany Karam on 5/2/20.
+//  Copyright © 2020 Hany Karam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
